773df315bca3c8a9bc8dd3464b1f72968674fb63
MrAFerreira <andre.f.v.ferreira@gmail.com>
2022-01-25 12:54:48 +0000

    adds rest of w2d2

--- Week_2/Day_2/dom/script.js
+++ Week_2/Day_2/dom/script.js
@@ -51,7 +51,7 @@ parent.removeChild(paragraph);
 //clear an element
 
 let infoDiv = document.getElementById('info');
-infoDiv.innerHTML = '';
+//infoDiv.innerHTML = '';
 
 let secondDiv = document.getElementById('second');
 secondDiv.innerHTML = `<h1>This was added trough javascript</h1>`;
@@ -60,9 +60,9 @@ secondDiv.innerHTML = `<h1>This was added trough javascript</h1>`;
 
 let button = document.getElementById('add-item');
 
-button.onclick = function () {
+/* button.onclick = function () {
   console.log('Button was clicked');
-};
+}; */
 h1Tag.onclick = function () {
   console.log('H1 was clicked');
 };
@@ -70,3 +70,35 @@ h1Tag.onclick = function () {
 // When the button is clicked:
 //Create an li inside of the list. The li should have inside:
 // Item Number (number)
+
+let input = document.getElementsByTagName('input')[0];
+console.log('input value:', input.value);
+
+let sendBtn = document.getElementById('send-btn');
+
+sendBtn.onclick = function () {
+  console.log(input.value);
+  input.value = '';
+};
+
+let list = document.getElementById('item-list');
+
+const liTags = document.getElementsByTagName('li');
+console.log(liTags);
+button.onclick = function () {
+  let item = document.createElement('li');
+  list.appendChild(item);
+  item.innerHTML = input.value;
+  item.onclick = function (event) {
+    console.log(event.currentTarget.innerHTML);
+    event.currentTarget.style.textDecoration = 'line-through';
+  };
+  input.value = '';
+};
+
+/* for (let i = 0; i < liTags.length; i++) {
+  liTags[i].onclick = function (event) {
+    console.log(event.currentTarget.innerHTML);
+  };
+}
+ */
